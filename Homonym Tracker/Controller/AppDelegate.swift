e908b78b2a30c34164042b7f5f78b66fe10afddc
ttreit <14933277+ttreit@users.noreply.github.com>
2019-06-20 14:58:34 -0700

    Add CocaoPods and Firebase

--- Homonym Tracker/Controller/AppDelegate.swift
+++ Homonym Tracker/Controller/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase //comment this line out if you're using a different DB
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure() //comment this line out if you're using a different DB
+        
         // Override point for customization after application launch.
         return true
     }
